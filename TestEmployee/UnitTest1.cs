46aaf8726e9d5c35dd1808d8df3b9d844f8b06d8
astridrodriguez <astridcarolina.rodriguez@amaris.com>
2023-05-25 10:57:55 -0500

    pruebas unitarias

--- TestEmployee/UnitTest1.cs
+++ TestEmployee/UnitTest1.cs
@@ -12,30 +12,41 @@ namespace TestEmployee
         public void GetEmployeesTest()
         {
             // Arrange
-            string urlService = "https://dummy.restapiexample.com/api/v1/employees";
+            string urlService = "https://dummy.restapiexample.com/api/v1/employee/1";
             List<EmployeeService> mockedEmployees = new List<EmployeeService>
             {
             new EmployeeService  { Id= "1" ,EmployeeAge = "23", EmployeeName = "Astrid", ProfileImage = "", EmployeeSalary = "44444"},
-            new EmployeeService  { Id= "2" ,EmployeeAge = "45", EmployeeName = "Carolina", ProfileImage = "", EmployeeSalary = "44444"}
             };
 
-            // Mock the ServiceEmployees dependency
             var mockServiceEmployees = new Mock<IServiceEmployees>();
             mockServiceEmployees.Setup(se => se.GetEmployees(urlService)).Returns(mockedEmployees);
 
 
-            // Create an instance of EmployeeBusiness and inject the mocked dependency
             var employeeBusiness = new EmployeeBusiness();
+
+            List<Employee> employees = new List<Employee>
+            {
+            new Employee  {Id = 1, AgeEmployee = 23, NameEmployee = "Tiger Nixon", ProfileImage = "", Salary = 320800, MonthlySalary = 2333333}            
+            };
 
             // Act
             List<Employee> result = employeeBusiness.GetEmployees(urlService);
 
             // Assert
             Assert.NotNull(result);
+            Assert.Equal(mockedEmployees.Count, result.Count);
+            Assert.All(result, employee =>
+            {
+                Assert.Equal(typeof(int), employee.Id.GetType());
+                Assert.Equal(typeof(string), employee.NameEmployee.GetType());
+                Assert.Equal(typeof(decimal), employee.Salary.GetType());
+                Assert.Equal(typeof(decimal), employee.MonthlySalary.GetType());
+                Assert.Equal(typeof(string), employee.ProfileImage.GetType());
+                Assert.Equal(typeof(int), employee.AgeEmployee.GetType());
+            });
 
         }
 
-
-
     }
+
 }
